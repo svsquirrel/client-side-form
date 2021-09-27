e37186daae8f4ba2e5e02f4b66cc19b3944b8080
svsquirrel <suekittle@hotmail.com>
2021-09-27 18:08:22 -0500

    just working on event listeners

--- index.js
+++ index.js
@@ -2,38 +2,39 @@
 const pagecontent = document.querySelector('.page-content');
 const form = document.querySelector('.existing-user');
 const about = document.querySelector('.about-page');
-    about.style.display = 'none';
 const shop = document.querySelector('.shop-page');
-    shop.style.display = 'none';
 const contact = document.querySelector('.contact-page');
-    contact.style.display = 'none';
 const createform = document.querySelector('.new-user');
+const items = document.querySelectorAll('.pageitem');
 
 const create = document.querySelector('.create-account');
-    create.addEventListener('click', () =>{
-        displayPage(createform);
-    });
+create.addEventListener('click', () => {
+    displayPage(createform);
+});
 const login = document.querySelector('.login');
-    login.addEventListener('click', () =>{
-          displayPage(form); 
-    });
+login.addEventListener('click', () => {
+    displayPage(form);
+});
 const navabout = document.querySelector('.about')
-    navabout.addEventListener('click', () => {
-        displayPage(about);
-    });
+navabout.addEventListener('click', () => {
+    displayPage(about);
+});
 const navshop = document.querySelector('.shop')
-    navshop.addEventListener('click', () => {
-        displayPage(shop);
-    });
+navshop.addEventListener('click', () => {
+    displayPage(shop);
+});
 const navcontact = document.querySelector('.contact')
-    navcontact.addEventListener('click', () => {
-        displayPage(contact);
-    });
+navcontact.addEventListener('click', () => {
+    displayPage(contact);
+});
+const returnbtn = document.querySelector('#return');
+returnbtn.addEventListener('click', () => {
+    displayPage(shop);
+    console.log('clock');
+})
+function displayPage(page) {
 
-function displayPage(page){
-   const items = document.querySelectorAll('.pageitem');
-        
-    if (page.style.display === 'none' ){
+    if (page.style.display === 'none') {
         items.forEach(item => {
             item.style.display = 'none';
         })
@@ -43,3 +44,9 @@ function displayPage(page){
     }
 }
 
+function loadPage() {
+    items.forEach(item => {
+        item.style.display = 'none';
+    })
+}
+loadPage()
