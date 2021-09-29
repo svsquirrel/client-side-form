92c975b384c33218aa8e016aa2de4b8340091ef3
svsquirrel <suekittle@hotmail.com>
2021-09-29 13:53:31 -0500

    got some more buttons working. need to style new user form

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 const pagecontent = document.querySelector('.page-content');
-const form = document.querySelector('.existing-user');
-const about = document.querySelector('.about-page');
-const shop = document.querySelector('.shop-page');
-const contact = document.querySelector('.contact-page');
-const createform = document.querySelector('.new-user');
-const items = document.querySelectorAll('.pageitem');
+const form        = document.querySelector('.existing-user');
+const createform  = document.querySelector('.new-user');
+const about       = document.querySelector('.about-page');
+const shop        = document.querySelector('.shop-page');
+const contact     = document.querySelector('.contact-page');
+const items       = document.querySelectorAll('.pageitem');
 
 const create = document.querySelector('.create-account');
 create.addEventListener('click', () => {
     displayPage(createform);
 });
-const login = document.querySelector('.login');
+const login = document.querySelector('.login')
 login.addEventListener('click', () => {
     displayPage(form);
 });
@@ -27,13 +27,24 @@ const navcontact = document.querySelector('.contact')
 navcontact.addEventListener('click', () => {
     displayPage(contact);
 });
-const returnbtn = document.querySelector('#return');
+const returnbtn = document.querySelector('.return')
 returnbtn.addEventListener('click', () => {
     displayPage(shop);
-    console.log('clock');
+    
 })
+/*---------FORM VALIDATION---------------*/
+const emails = document.querySelectorAll('.email');
+    emails.forEach(email =>{
+        email.addEventListener('input', function(event){
+            if (email.validity.typeMismatch){
+                email.setCustomValidity('Please enter a valid email address')
+            }else {
+                email.setCustomValidity('');
+            }
+        })
+    })
+/*---------DISPLAY FUNCTIONS-------------*/
 function displayPage(page) {
-
     if (page.style.display === 'none') {
         items.forEach(item => {
             item.style.display = 'none';
@@ -49,4 +60,5 @@ function loadPage() {
         item.style.display = 'none';
     })
 }
+
 loadPage()
